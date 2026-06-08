eb0177613f87995d55012d91a97d79c4cae42bfa
Nikhil Kurane <143862845+nikhil25nik@users.noreply.github.com>
2026-06-08 22:38:12 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL || "mongodb+srv://nikhilkurane25_db_user:Nikhil1234@cluster0.n18mkqu.mongodb.net/webCraft?appName=Cluster0");
         console.log("Connected DB");
     }catch(err){
         console.log(err);
