06b6f608554222e967bbe19c026e77fdbcfa3ac5
Nikhil Kurane <143862845+nikhil25nik@users.noreply.github.com>
2026-06-09 13:19:42 +0530

    Remove demo route from websiteRouter
    Removed demo route from websiteRouter.

--- server/routes/website.route.js
+++ server/routes/website.route.js
@@ -1,15 +1,12 @@
 import express from "express"
 import { changes, deploy, generateWebsite, getAll, getBySlug, getWebsiteById } from "../controller/website.controller.js";
 import { isAuth } from "../middleware/isAuth.js";
-import { demoController } from "../controller/user.controller.js";
-
 const websiteRouter = express.Router();
 
 websiteRouter.post("/generate",isAuth, generateWebsite)
 websiteRouter.post("/update/:id",isAuth, changes)
 websiteRouter.get("/get-by-id/:id",isAuth, getWebsiteById)
 websiteRouter.get("/getAll",isAuth, getAll)
-websiteRouter.post("/demo",demoController)
 websiteRouter.get("/deploy/:id",isAuth,deploy)
 websiteRouter.get("/get-by-slug/:slug",getBySlug)
 
