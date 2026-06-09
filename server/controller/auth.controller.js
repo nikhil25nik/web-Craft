cd6a7534a10d8f246daa7edac98615a8738f7612
Nikhil Kurane <143862845+nikhil25nik@users.noreply.github.com>
2026-06-09 12:21:45 +0530

    Update auth.controller.js

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -17,12 +17,12 @@ export const googleAuth = async(req,res)=>{
 
         let token = await jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"7d"});
 
-        res.cookie("token",token,{
-            httpOnly:true,
-            secure:false,
-            sameSite:"strict",
-            maxAge:7*24*60*60*1000
-        })
+       res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+})
 
         return res.status(200).json(user);
 
@@ -35,8 +35,8 @@ export const logOut = async(req,res)=>{
     try{
          res.clearCookie("token",{
             httpOnly:true,
-            secure:false,
-            sameSite:"strict"
+            secure:true,
+            sameSite:"none"
     })
 
     res.status(200).json({message:"Logout Successfully"})
