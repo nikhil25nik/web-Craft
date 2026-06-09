d3da321a50c490e5dde50491c0501cec24d0a1ee
Nikhil Kurane <143862845+nikhil25nik@users.noreply.github.com>
2026-06-09 12:38:07 +0530

    Remove demoController from user controller
    Removed demoController function from user.controller.js.

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -11,13 +11,3 @@ export const getCurrentUser = async(req,res)=>{
     }
 }
 
-export const demoController = async(req,res)=>{
-    try{
-        const data = await generateResponse("hello");
-        const result = data.choices[0].content
-        return res.status(200).json(data)
-    }catch(error){
-        return res.status(500).json(error)
-        
-    }
-}
